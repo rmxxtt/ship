da7d20f4764ad6a14592aa93703556b4a0e3ee4e
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2022-06-30 23:30:24 +0700

    add func

--- app/main.py
+++ app/main.py
@@ -8,6 +8,14 @@ class Ship(enum.Enum):
     MAX = 4  # максимальная длина сторон корабля
 
 
+class LineDirection(enum.Enum):
+    # Направление стороны корабля для функции поиска
+    LEFT = 1
+    RIGHT = 2
+    TOP = 3
+    BOTTOM = 4
+
+
 def load_fields():
     # Загружаем поля
     with open('./fields.json') as json_file:
@@ -33,8 +41,46 @@ def draw_fields(fields: list):
         print()
 
 
+def is_outside_field(x, y, h, w) -> bool:
+    # Проверяем, выходят ли координаты за пределы поля
+    if (0 > x > w) or (0 > y > h):
+        return False
+    else:
+        return True
+
+
+def walk_the_line(X: int, Y: int, direction: LineDirection, field: dict):
+    to_x = 0
+    to_y = 0
+    match direction:
+        case LineDirection.LEFT:
+            to_x -= 1
+        case LineDirection.RIGHT:
+            to_x += 1
+        case LineDirection.TOP:
+            to_y -= 1
+        case LineDirection.BOTTOM:
+            to_y += 1
+    x = X + to_x
+    y = Y + to_y
+    if not is_outside_field(x, y, field["h"], field["w"]):
+        pass
+
+
+def algorithm_1(field: dict):
+    for Y in range(field["h"]):
+        for X in range(field["w"]):
+            # Если * - начинаем двигаться по сторонам
+            if field["field"][Y][X] == "*":
+                # Влево
+                y, x = Y, X - 1
+                if x >= 0:
+                    pass
+
+
 def main():
     data = load_fields()
+    algorithm_1(data[1])
 
 
 if __name__ == '__main__':
